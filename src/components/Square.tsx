21016af26440a3696e09a2f6d94332fc5aa75eaa
lleonori <lorenzo.leonori.93@gmail.com>
2025-09-01 09:25:07 +0200

    Init project

---
+++ src/components/Square.tsx
@@ -0,0 +1,26 @@
+export type SquareProps = {
+  value: string;
+  onSquareClick?: () => void;
+};
+
+export function Square({ value, onSquareClick }: SquareProps) {
+  return (
+    <button
+      style={{
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 0,
+        backgroundColor: "#fff",
+        border: "1px solid #999",
+        outline: 0,
+        borderRadius: 0,
+        fontSize: "1rem",
+        fontWeight: "bold",
+      }}
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
